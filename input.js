9fa4f3f77da063ab5acf027d7c288648f80f93bb
Steven <schenn1992@gmail.com>
2020-11-04 23:38:32 +0000

    WASD movement

--- input.js
+++ input.js
@@ -1,18 +1,34 @@
+const { Console } = require("console");
+
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   const handleUserInput = stdin.on('data', (key) => {
+    
     if (key === '\u0003') {
       process.exit();
     }
-  });
+    if (key === 'w') {
+      console.log('up')
+    }; 
+    if (key === 's') {
+      console.log('down')
+    }; 
+    if (key === 'd') {
+      console.log('right')
+    }; 
+    if (key === 'a') {
+      console.log('left')
+    }; 
+  })
   return stdin;
-}
+};
 
 let exportFunc = {
   setupInput,
 };
 
+setupInput();
 module.exports = exportFunc;
