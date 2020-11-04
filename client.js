10fb64b1030bc3baea5173c2b088b6f1a9e35a31
Steven <schenn1992@gmail.com>
2020-11-04 20:01:12 +0000

    send move up to server

--- client.js
+++ client.js
@@ -8,8 +8,13 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
   conn.on('connect', () => console.log("Successfully connected to game server"));
-  conn.on('connect', () => conn.write("Name: YHC"));
+  conn.on('connect', () => conn.write("Move: up"));
   return conn;
 
 }
