68a1b538266b51bb8bfd6ba4720b65996862a3ac
Steven <schenn1992@gmail.com>
2020-11-04 20:55:37 +0000

    export setupInput func

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+}
+
+let exportFunc = {
+  setupInput,
+};
+
+module.exports = exportFunc;
