21e4bbe67d9b12a1cfc43c458d2570fe89c594c3
Steven <schenn1992@gmail.com>
2020-11-04 21:14:22 +0000

    remove setupInput

--- play.js
+++ play.js
@@ -1,17 +1,7 @@
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  return stdin;
-}
-
+const { setupInput } = require('./input');
 const { connect } = require('./client');
+
 console.log('Connecting ...');
 connect();
+
 setupInput();
